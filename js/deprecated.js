3a9419d22d79da9e1b38b3b525096c22b6dab789
rrbrink <ryanbrinkerhoff@live.com>
2018-12-05 14:47:31 -0700

    first commit

---
+++ js/deprecated.js
@@ -0,0 +1,8 @@
+
+
+var deprecated = {
+	configs: ["kioskmode"],
+};
+
+/*************** DO NOT EDIT THE LINE BELOW ***************/
+if (typeof module !== "undefined") {module.exports = deprecated;}
---
+++ js/utils.js
@@ -0,0 +1,13 @@
+
+
+var colors = require("colors/safe");
+
+var Utils = {
+	colors: {
+		warn: colors.yellow,
+		error: colors.red,
+		info: colors.blue
+	}
+};
+
+if (typeof module !== "undefined") {module.exports = Utils;}
